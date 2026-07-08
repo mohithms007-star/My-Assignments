7f097fd336683a7b1f195c8fcbdf8c69713f04a8
mohithms007-star <mohith.ms007@gmail.com>
2026-07-08 17:19:00 +0530

    Add files via upload
    Week1-day1

---
+++ DAY1.ts
@@ -0,0 +1,19 @@
+let firstName = "MohithMS"
+console.log( firstName )
+console.log(typeof firstName)
+
+let companyName="Side.INC"
+console.log(companyName);
+console.log(typeof companyName);
+
+let mobileNumber=7204737417
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+
+let isAutomation=true
+console.log(isAutomation);
+console.log(typeof isAutomation);
+
+let hasPlaywright=
+console.log();
+console.log(typeof hasPlaywright);
